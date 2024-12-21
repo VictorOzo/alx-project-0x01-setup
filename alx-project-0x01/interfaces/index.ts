507c43672f2e34c81602984f05a54de47e0b1500
Ozokwelu Victor <ozokweluvic@gmail.com>
2024-12-21 04:09:55 -0800

    errors fixed

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -44,7 +44,7 @@ export interface UserProps {
 
 export interface UserModalProps {
   onClose: () => void;
-  onSubmit: (user: UserProps) => void;
+  onSubmit: (post: UserProps) => void;
 }
 
 export interface PostModalProps {
